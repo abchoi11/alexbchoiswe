73d1b2518e92c44b204cd87e5629a612483aec66
abchoi11 <alexander_b_choi@brown.edu>
2025-02-20 19:24:38 -0500

    added capital one experience

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Section from "../components/Experience/Section/Section";
 import Header from "../components/Header/Header";
 import NavigationBar from "../components/Navbar/NavigtionBar";
+import capitalone from "../images/capitalone.png"
 import brightspot from "../images/brightspot.png";
 import brown from "../images/brown.png";
 import sciquel from "../images/sciquel.png";
@@ -25,10 +26,21 @@ function Experience(props) {
         author={"Of the Dignity or Meanness of Human Nature | David Hume"}
       />
       <Section
+        logo={capitalone}
+        title={"Software Engineer Intern"}
+        company={"Capital One"}
+        date={"May 2024 - August 2024"}
+        description={[
+          "Developed a full-stack application via a Jenkins pipeline that serves over 200 associates by consolidating project information into a single interface and reducing on-boarding efforts by 25\%",
+          "Created Node.js REST API with Postman and Insomnia API build tools to authenticate proper validation and permission handling that fetches, updates, and creates company data via AWS Aurora SQL Database",
+          "Developed dynamic micro-frontend using Vue and Vuex, incorporating real-time design changes, and  integrated it with the Node.js back-end API capable of handling high-volume of concurrent requests"
+        ]}
+      />
+      <Section
         logo={brightspot}
         title={"Software Engineer Intern"}
         company={"Brightspot"}
-        date={"May 2023 - Present"}
+        date={"May 2023 - August 2023"}
         description={[
           "Utilized Java framework and Docker to deliver client-facing CMS service for Brigham Young University networks",
           "Responded and resolved real time bug tickets for CMS that supports over 36,000 users on a daily basis across 900 sites",
@@ -38,7 +50,7 @@ function Experience(props) {
         logo={brown}
         title={"Undergraduate Teaching Assistant"}
         company={"Brown University"}
-        date={"January 2023 - May 2023"}
+        date={"January 2023 - January 2025"}
         description={[
           "Created lecture slides visualizing how the RANSAC algorithm bounds in multidimensional spaces and how it applies to image reconstruction",
           "Implemented a RANSAC algorithm visualizer to allow 150+ students to be able to monitor their camera geometry estimation of the fundamental matrix",
